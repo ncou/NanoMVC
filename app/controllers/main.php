1240025752143f7d280dfb55ce2587f1de3d0b78
ncou <nicolas_courade@yahoo.fr>
2017-01-23 12:41:54 +0100

    update the example

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -2,26 +2,36 @@
 
 class Main extends Controller {
 
+	private $example;
+	private $layout;
+	private $response;
+
 	function __construct() {
 		// uncomment to access the model database.
-        //$this->model = new Example_model();
-        $this->view = new View();
+        //$this->example = $this->model('example_model');
+        $this->view = $this->view();
+        $this->response = $this->helper('response_helper');
 	}
 
 	function index()
 	{
 		// uncomment to read the database
-		//$res = $this->model->getSomething(123);
+		//$res = $this->example->getSomething(123);
 
-		// uncomment to test the attrabutes who could be used from the view page
-		//$this->view->addAttribute('toto',$res);
-		$this->view->renderHtml('main_view.phtml');
+		// uncomment to test the attributes who could be used from the view page
+		//$this->view->addAttribute('test',$res);
+		$this->view->layout('main_view.phtml');
+
+		//$this->response->status(HTTP_OK);
+		$this->response->writeHTML($this->view);
 		// uncomment to test a Json or XML render (used for a REST application)
-		//$this->view->renderXml();
-		//$this->view->renderJson();
+		//$this->response->writeJSON($this->view);
+		//$this->view->rootName('ROOT_NODE');
+		//$this->response->writeXML($this->view);
 
 		// uncomment to test a redirection
 		//$this->redirect('login/success/');
+
 		
 	}
     
